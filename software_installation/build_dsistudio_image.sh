fda479e3f6138d24a1ae62794f71d69e166206c2
Valerie Sydnor <valeriejill.sydnor@gmail.com>
2023-11-22 13:15:43 -0500

    initiate gh-pages

--- software_installation/build_dsistudio_image.sh
+++
@@ -1,6 +0,0 @@
-#build a singularity image for dsi-studio
-
-#docker: https://hub.docker.com/r/dsistudio/dsistudio
-#built on 02/18/2023, https://hub.docker.com/layers/dsistudio/dsistudio/chen-2023-02-17/images/sha256-fa4b86442032f1cf993e5ab975068aaf10f5c87679406544d1820ce3ec1f461a?context=explore
-
-singularity build /cbica/projects/thalamocortical_development/software/dsistudio-latest.simg docker://dsistudio/dsistudio:latest
--- software_installation/build_qsiprep_image.sh
+++
@@ -1,6 +0,0 @@
-#build a singularity image for qsiprep
-
-#docker: https://hub.docker.com/r/pennbbl/qsiprep
-#tag: https://hub.docker.com/layers/pennbbl/qsiprep/0.18.1/images/sha256-7699702ae06f82ec268166bcaca0f2f965f1ebe2c034fa7bc5f96e411ca00322?context=explore
-
-singularity build /cbica/projects/thalamocortical_development/software/qsiprep-0.18.1.simg docker://pennbbl/qsiprep:0.18.1
