25bd1e9365b8f5b0e3cbe712722a396a9cf5cf58
Valerie Sydnor <valeriejill.sydnor@gmail.com>
2023-12-03 14:43:06 -0500

    qsiprep and recon calls

---
+++ qsiprep/HCPD/qsiprep_call_HCPD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -u -x
+
+subid="$1"
+
+mkdir -p ${PWD}/.git/tmp/wdir
+singularity run --cleanenv -B ${PWD} \
+    pennlinc-containers/.datalad/environments/qsiprep-0-16-1/image \
+    inputs/data \
+    prep \
+    participant \
+    -v -v \
+    -w ${PWD}/.git/wkdir \
+    --n_cpus $NSLOTS \
+    --stop-on-first-crash \
+    --fs-license-file code/license.txt \
+    --skip-bids-validation \
+    --participant-label "$subid" \
+    --unringing-method mrdegibbs \
+    --output-resolution 1.5
+
+cd prep
+7z a ../${subid}_qsiprep-0.16.1.zip qsiprep
+rm -rf prep .git/tmp/wkdir
+
